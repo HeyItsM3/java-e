23ec02887bc0f90b18b726ba5577a45c56d6d9a8
Aldo <aldoyedro181@gmail.com>
2021-08-14 10:22:45 -0300

    Nuevos ejercicios

---
+++ EstudioED/src/paquete/Cola.java
@@ -0,0 +1,35 @@
+package paquete;
+
+public class Cola {
+	Cola primero, ultimo;
+	int tamanio;
+	
+	public Cola() {
+		primero = ultimo = null;
+		tamanio = 0;
+	}
+	
+	public Cola(int valor) {
+		
+	}
+	
+	
+	public void insert(int valor) {
+		Cola nodo = new Cola(valor);
+		
+	}
+
+	public void delete() {
+			
+	}
+	
+	public void size() {
+		
+	}
+	
+	public void empty() {
+		
+	}
+
+
+}
--- EstudioED/src/paquete/Pila.java
+++ EstudioED/src/paquete/Pila.java
@@ -1,5 +1,55 @@
 package paquete;
 
-public class Pila {
+import java.util.ArrayList;
 
+public class Pila {
+	private ArrayList<Object> pila = new ArrayList <>();
+	
+	private void push(Object elemento) {
+		pila.add(elemento);
+	}
+	
+	private Object pop() {
+		if(!pila.isEmpty()) {
+			Object t = pila.get(pila.size()-1);
+			pila.remove(pila.size()-1);
+			return t;
+		}
+		else 
+			return null;
+	}
+	
+	private Object peek() {
+		if(!pila.isEmpty()) {
+			return pila.get(pila.size()-1);
+		}
+		else
+			return null;
+	}
+	
+	private Boolean empty() {
+		return pila.isEmpty();
+	}
+	
+	private int size() {
+		return pila.size();
+	}
+	
+	
+	
+	
+	public static void main(String[] args){
+		Pila pila = new Pila();
+		pila.push("hola");
+		pila.push(1);
+		pila.push(3);
+		pila.push("Nab");
+		pila.push(123546);
+		pila.push(true);
+		pila.pop();
+		System.out.println(pila.peek());
+		System.out.println(pila.size());
+		System.out.println(pila.empty());
+		
+	}
 }
