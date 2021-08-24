9d3cd12fd429303542b9e41358a6b7c74cbf585d
Aldo <aldoyedro181@gmail.com>
2021-08-24 17:14:23 -0300

    Edit a PonyE

--- Pony/src/ponyE/PonyExpress.java
+++ Pony/src/ponyE/PonyExpress.java
@@ -4,15 +4,19 @@ public class PonyExpress {
 	public static int jinetes(int[] distancias) {
 		  int caballo=1, millas=0;
 		  
-		  for(int i=0; i<distancias.length; i++){
+		  for(int i=0; i < distancias.length; i++){
 			  
 			  millas += distancias[i];
+			  
 		     if (millas > 100){
 		    	 
-		    	 millas=distancias[i];
+		    	 millas = distancias[i];
+		    	 
 		         caballo+=1;
 		      }
+		     
 		     }
+		  
 		   return caballo;
 	}
 }
