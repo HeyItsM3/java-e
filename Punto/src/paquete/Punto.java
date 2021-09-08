5c3d244c95620a2fea1d9eed88e28c5b02fc5a3a
Aldo <aldoyedro181@gmail.com>
2021-09-08 16:21:10 -0300

    Agregar Ejercicios Faltantes

--- Punto/src/paquete/Punto.java
+++ Punto/src/paquete/Punto.java
@@ -1,4 +1,4 @@
-package circec;
+package paquete;
 
 public class Punto {
 	private double coordenadaX;
@@ -11,10 +11,12 @@ public class Punto {
 
 	public double obtenerX() {
 		return this.coordenadaX;
+
 	}
 
 	public double obtenerY() {
 		return this.coordenadaY;
+
 	}
 
 	public void cambiarX(double nuevoX) {
@@ -64,23 +66,3 @@ public class Punto {
 
 }
 
-
-// public class Punto {
-// 	public void AsignarCoordenadasAlCentro1 (double Coordenada1,double Coordenada2) {
-// 		System.out.println("-> El primer Centro tiene coordenas:");
-// 		System.out.println(Coordenada1);
-// 		System.out.println(Coordenada2);
-// 	}
-// 	public void AsignarCoordenadasAlCentro2 (double Coordenada3,double Coordenada4) {
-// 		System.out.println("-> El Segundo Centro tiene coordenas:");
-// 		System.out.println(Coordenada3);
-// 		System.out.println(Coordenada4);
-// 	}
-// 	public double CalcularDistanciaEntreCentros (double X1,double Y1,double X2,double Y2) {
-// 		double Distancia;
-		
-// 		Distancia = Math.sqrt(Math.pow(X2-X1,2)+Math.pow(Y2-Y1,2));
-		
-// 		return Distancia;
-// 	}
-// }
